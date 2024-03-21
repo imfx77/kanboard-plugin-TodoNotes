92aa91dc53513f05fc9240359e0403eaf2a1e285
Im[F(x)] <imfx77@hotmail.com>
2024-03-21 04:27:50 +0200

    * non-spinning icon for 0 notes in progress

--- Template/widgets/stats.php
+++ Template/widgets/stats.php
@@ -6,8 +6,11 @@
 
     $statsData = $this->model->boardNotesModel->boardNotesStats($stats_project_id, $stats_user_id);
 
+
     $statsWidget = '<span class="BoarNotes_StatsWidget" id="BoarNotes_StatsWidget_P' . $stats_project_id . '">';
-    $statsWidget .= '<span class="statProgress" title="'.t('Progress').'"><i class="fa fa-fw fa-spinner fa-pulse" aria-hidden="true"></i><b>' . $statsData['statProgress'] . '</b></span>';
+    $statsWidget .= '<span class="statProgress" title="'.t('Progress').'"><i class="fa fa-fw fa-spinner ';
+    $statsWidget .= ($statsData['statProgress'] > 0) ? 'fa-pulse' : '';
+    $statsWidget .= '" aria-hidden="true"></i><b>' . $statsData['statProgress'] . '</b></span>';
     $statsWidget .= '<span class="statDone" title="'.t('Done').'"><i class="fa fa-fw fa-check" aria-hidden="true"></i><b>' . $statsData['statDone'] . '</b></span>';
     $statsWidget .= '<span class="statOpen" title="'.t('Open').'"><i class="fa fa-fw fa-circle-thin" aria-hidden="true"></i><b>' . $statsData['statOpen'] . '</b></span>';
     $statsWidget .= '</span>';
