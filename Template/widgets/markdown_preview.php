78df123ee1a5627d9a324bd8b83bc01e72167fe7
Im[F(x)] <imfx77@hotmail.com>
2024-04-27 09:40:57 +0300

    *  fix linter errors

--- Template/widgets/markdown_preview.php
+++ Template/widgets/markdown_preview.php
@@ -1,7 +1,7 @@
 <?php
 
-    if (!empty($markdown_text)) {
-        print $this->helper->text->markdown($markdown_text);
-    } else {
-        print t('BoardNotes_PROJECT_NOTE_DETAILS_EDIT_HINT');
-    }
+if (!empty($markdown_text)) {
+    print $this->helper->text->markdown($markdown_text);
+} else {
+    print t('BoardNotes_PROJECT_NOTE_DETAILS_EDIT_HINT');
+}
