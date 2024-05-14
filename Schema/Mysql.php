0b898d78e7700ee6f3d2d7ee7a3313e9ba52185b
Im[F(x)] <imfx77@hotmail.com>
2024-05-14 23:02:02 +0300

    * made sure reindexing of lists and notes adds that special id=0 record in the `boardnotes_entries` table!

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -37,12 +37,9 @@ function version_1(PDO $pdo)
     ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
 
     $pdo->exec('INSERT INTO `boardnotes_custom_projects`(`owner_id`, `position`, `project_name`) VALUES (0, 1, "Global Notes")');
-
     $pdo->exec('INSERT INTO `boardnotes_custom_projects`(`owner_id`, `position`, `project_name`) VALUES (0, 2, "Global TODO")');
 
-    $pdo->exec('INSERT INTO `boardnotes_entries`
-                (`id`, `project_id`, `user_id`, `position`, `is_active`, `title`, `category`, `description`, `date_created`, `date_modified`)
-                VALUES (0, 0, 0, 0, -1, "", "", "", 0, 0)');
+    $pdo->exec('INSERT INTO `boardnotes_entries` (`id`, `project_id`, `user_id`, `position`, `is_active`) VALUES (0, 0, 0, 0, -1)');
 }
 
 //------------------------------------------------
@@ -61,6 +58,7 @@ function reindexNotesAndLists_1(PDO $pdo)
         `date_modified` INT
     ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
 
+    $pdo->exec('INSERT INTO `boardnotes_entries_NOPK` (`id`, `project_id`, `user_id`, `position`, `is_active`) VALUES (0, 0, 0, 0, -1)');
     $pdo->exec('INSERT INTO `boardnotes_entries_NOPK`
                 SELECT * FROM `boardnotes_entries`
                 WHERE `project_id` <> 0 AND `user_id` > 0 AND `position` > 0 AND `is_active` >= 0');
--- Schema/Postgres.php
+++ Schema/Postgres.php
@@ -35,12 +35,9 @@ function version_1(PDO $pdo)
     )');
 
     $pdo->exec('INSERT INTO boardnotes_custom_projects (owner_id, position, project_name) VALUES (0, 1, "Global Notes")');
-
     $pdo->exec('INSERT INTO boardnotes_custom_projects (owner_id, position, project_name) VALUES (0, 2, "Global TODO")');
 
-    $pdo->exec('INSERT INTO boardnotes_entries
-                (id, project_id, user_id, position, is_active, title, category, description, date_created, date_modified)
-                VALUES (0, 0, 0, 0, -1, "", "", "", 0, 0)');
+    $pdo->exec('INSERT INTO boardnotes_entries (id, project_id, user_id, position, is_active) VALUES (0, 0, 0, 0, -1)');
 }
 
 //------------------------------------------------
@@ -59,6 +56,7 @@ function reindexNotesAndLists_1(PDO $pdo)
         date_modified INTEGER
     )');
 
+    $pdo->exec('INSERT INTO boardnotes_entries_NOPK (id, project_id, user_id, position, is_active) VALUES (0, 0, 0, 0, -1)');
     $pdo->exec('INSERT INTO boardnotes_entries_NOPK
                 SELECT * FROM boardnotes_entries
                 WHERE project_id <> 0 AND user_id > 0 AND position > 0 AND is_active >= 0');
