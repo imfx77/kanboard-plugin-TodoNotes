6339bab06aa204b7cf170f41799b1a7329603e01
Im[F(x)] <imfx77@hotmail.com>
2024-05-18 13:56:42 +0300

    * taking into account the `optionShowAllDone` for the report

--- Template/project/report.php
+++ Template/project/report.php
@@ -46,88 +46,90 @@ print '></div>';
 $num = "1";
 
 foreach ($data as $u) {
-    print '<tr class="trReport" id="trReportNr' . $num . '">';
+    if ($optionShowAllDone || $u['is_active'] != "0") {
+        print '<tr class="trReport" id="trReportNr' . $num . '">';
 
-    print '<td class="tdReport tdReportNr">';
-    print '<div class="reportBkgr"></div>';
+        print '<td class="tdReport tdReportNr">';
+        print '<div class="reportBkgr"></div>';
 
-     // Hide button
-    print '<button id="reportHide" class="reportHide"';
-    print ' data-id="' .  $num . '"';
-    print '>';
-    print '<i class="fa fa-minus-square-o" style="color:#CCCCCC" aria-hidden="true"';
-    print ' title="' . t('BoardNotes_REPORT_HIDE_ROW') . '">';
-    print '</i>';
-    print '</button>';
-    // Report #
-    print '<span class="fa-stack fa-lg">';
-    print '<i class="fa fa-circle-thin fa-stack-2x"></i>';
-    print '<i class="fa fa-inverse fa-stack-1x">' .  $num . '</i>';
-    print '</span>';
-    print '</td>';
-
-    // Report Info
-    print '<td class="tdReport tdReportInfo">';
-    print '<div class="reportBkgr"></div>';
-
-    // Note title label
-    print '<label id="reportTitleLabel-P' . $u['project_id'] . '-' . $num . '"';
-    if ($u['is_active'] == "0") {
-        print ' class="reportTitleLabel reportTitle noteDoneText">';
-    } else {
-        print ' class="reportTitleLabel reportTitle">';
-    }
-    print $u['title'];
-    print '</label>';
-
-    // Category label
-    print '<label class="catLabel containerFloatRight"';
-    print ' id="noteCatLabel-P' . $u['project_id'] . '-' . $num . '"';
-    print ' data-id="' . $num . '"';
-    print ' data-project="' . $u['project_id'] . '"';
-    print '>';
-    print $u['category'];
-    print '</label>';
-
-    // Note details
-    if (!empty($u['description'])) {
-        print '<div id="noteDetails-P' . $u['project_id'] . '-' . $num . '"';
-        print ' class="details reportDetails ui-corner-all">';
-
-        print '<span id="noteMarkdownDetails-P' . $u['project_id'] . '-' . $num . '"';
-        if ($u['is_active'] == "0") {
-            print ' class="markdown markdownReportDetails reportTitle noteDoneMarkdown"';
-        } else {
-            print ' class="markdown markdownReportDetails reportTitle"';
-        }
+        // Hide button
+        print '<button id="reportHide" class="reportHide"';
+        print ' data-id="' . $num . '"';
         print '>';
-        print $this->helper->text->markdown($u['description']);
+        print '<i class="fa fa-minus-square-o" style="color:#CCCCCC" aria-hidden="true"';
+        print ' title="' . t('BoardNotes_REPORT_HIDE_ROW') . '">';
+        print '</i>';
+        print '</button>';
+        // Report #
+        print '<span class="fa-stack fa-lg">';
+        print '<i class="fa fa-circle-thin fa-stack-2x"></i>';
+        print '<i class="fa fa-inverse fa-stack-1x">' . $num . '</i>';
         print '</span>';
+        print '</td>';
 
-        print '</div>';
+        // Report Info
+        print '<td class="tdReport tdReportInfo">';
+        print '<div class="reportBkgr"></div>';
+
+        // Note title label
+        print '<label id="reportTitleLabel-P' . $u['project_id'] . '-' . $num . '"';
+        if ($u['is_active'] == "0") {
+            print ' class="reportTitleLabel reportTitle noteDoneText">';
+        } else {
+            print ' class="reportTitleLabel reportTitle">';
+        }
+        print $u['title'];
+        print '</label>';
+
+        // Category label
+        print '<label class="catLabel containerFloatRight"';
+        print ' id="noteCatLabel-P' . $u['project_id'] . '-' . $num . '"';
+        print ' data-id="' . $num . '"';
+        print ' data-project="' . $u['project_id'] . '"';
+        print '>';
+        print $u['category'];
+        print '</label>';
+
+        // Note details
+        if (!empty($u['description'])) {
+            print '<div id="noteDetails-P' . $u['project_id'] . '-' . $num . '"';
+            print ' class="details reportDetails ui-corner-all">';
+
+            print '<span id="noteMarkdownDetails-P' . $u['project_id'] . '-' . $num . '"';
+            if ($u['is_active'] == "0") {
+                print ' class="markdown markdownReportDetails reportTitle noteDoneMarkdown"';
+            } else {
+                print ' class="markdown markdownReportDetails reportTitle"';
+            }
+            print '>';
+            print $this->helper->text->markdown($u['description']);
+            print '</span>';
+
+            print '</div>';
+        }
+
+        print '</td>'; // report info
+
+        print '<td class="tdReport tdReportStatus reportTitle">';
+        print '<div class="reportBkgr"></div>';
+
+        if ($u['is_active'] == "2") {
+            print '<i class="fa fa-spinner fa-pulse" aria-hidden="true"></i>';
+        }
+        //if ($u['is_active'] == "1") {
+        //    print '<i class="fa fa-circle-thin" aria-hidden="true"></i>';
+        //}
+        if ($u['is_active'] == "0") {
+            print '<i class="fa fa-check" aria-hidden="true"></i>';
+        }
+
+        print '</td>';
+
+        print '</tr>';
+
+        // #
+        $num++;
     }
-
-    print '</td>'; // report info
-
-    print '<td class="tdReport tdReportStatus reportTitle">';
-    print '<div class="reportBkgr"></div>';
-
-    if ($u['is_active'] == "2") {
-        print '<i class="fa fa-spinner fa-pulse" aria-hidden="true"></i>';
-    }
-    //if ($u['is_active'] == "1") {
-    //    print '<i class="fa fa-circle-thin" aria-hidden="true"></i>';
-    //}
-    if ($u['is_active'] == "0") {
-        print '<i class="fa fa-check" aria-hidden="true"></i>';
-    }
-
-    print '</td>';
-
-    print '</tr>';
-
-    // #
-    $num++;
 }
 
 ?>
