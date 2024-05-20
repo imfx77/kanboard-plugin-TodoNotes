27b7a4442ffd17ec701294a3a42cf0a34b96db23
Im[F(x)] <imfx77@hotmail.com>
2024-05-20 11:28:46 +0300

    * reworked schema creation and reindexing statements - added comments, improved formatting and readability * implemented reindexing for custom projects (also cross updating the notes with reindexed custom projects) * keeping as minimal differences as possible btw SQLite, PostgreSQL and MySQL impls (this depends on the corresponding SQL impl specifics)

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -20,75 +20,110 @@ const VERSION = 1;
 //------------------------------------------------
 function version_1(PDO $pdo)
 {
+    // create+insert custom projects
     $pdo->exec('CREATE TABLE IF NOT EXISTS `boardnotes_custom_projects` (
-        `id` INT NOT NULL AUTO_INCREMENT,
-        `owner_id` INT NOT NULL DEFAULT 0,
-        `position` INT,
-        `project_name` TEXT,
-        PRIMARY KEY(id)
-    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
+                    `id` INT NOT NULL AUTO_INCREMENT,
+                    `owner_id` INT NOT NULL DEFAULT 0,
+                    `position` INT,
+                    `project_name` TEXT,
+                    PRIMARY KEY(id)
+                ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
+    $pdo->exec('INSERT INTO `boardnotes_custom_projects`
+                    (`owner_id`, `position`, `project_name`)
+                    VALUES (0, 1, "Global Notes")
+                ');
+    $pdo->exec('INSERT INTO `boardnotes_custom_projects`
+                    (`owner_id`, `position`, `project_name`)
+                    VALUES (0, 2, "Global TODO")
+                ');
 
+    // create+insert entries
     $pdo->exec('CREATE TABLE IF NOT EXISTS `boardnotes_entries` (
-        `id` INT NOT NULL AUTO_INCREMENT,
-        `project_id` INT NOT NULL,
-        `user_id` INT NOT NULL,
-        `position` INT,
-        `is_active` INT,
-        `title` TEXT,
-        `category` TEXT,
-        `description` TEXT,
-        `date_created` INT,
-        `date_modified` INT,
-        PRIMARY KEY(id)
-    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
-
-    $pdo->exec('INSERT INTO `boardnotes_custom_projects`(`owner_id`, `position`, `project_name`) VALUES (0, 1, "Global Notes")');
-    $pdo->exec('INSERT INTO `boardnotes_custom_projects`(`owner_id`, `position`, `project_name`) VALUES (0, 2, "Global TODO")');
-
-    $pdo->exec('INSERT INTO `boardnotes_entries` (`project_id`, `user_id`, `position`, `is_active`, `date_created`, `date_modified`) VALUES (0, 0, 0, -1, 0, 0)');
+                    `id` INT NOT NULL AUTO_INCREMENT,
+                    `project_id` INT NOT NULL,
+                    `user_id` INT NOT NULL,
+                    `position` INT,
+                    `is_active` INT,
+                    `title` TEXT,
+                    `category` TEXT,
+                    `description` TEXT,
+                    `date_created` INT,
+                    `date_modified` INT,
+                    PRIMARY KEY(id)
+                ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
+    $pdo->exec('INSERT INTO `boardnotes_entries`
+                    (`project_id`, `user_id`, `position`, `is_active`, `date_created`, `date_modified`)
+                    VALUES (0, 0, 0, -1, 0, 0)
+                ');
 }
 
 //------------------------------------------------
 function reindexNotesAndLists_1(PDO $pdo)
 {
-    $pdo->exec('CREATE TABLE IF NOT EXISTS `boardnotes_entries_NOPK` (
-        `id` INT NOT NULL,
-        `project_id` INT NOT NULL,
-        `user_id` INT NOT NULL,
-        `position` INT,
-        `is_active` INT,
-        `title` TEXT,
-        `category` TEXT,
-        `description` TEXT,
-        `date_created` INT,
-        `date_modified` INT
-    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
+    // add+update old_project_id
+    $pdo->exec('ALTER TABLE `boardnotes_custom_projects` ADD `old_project_id` INTEGER');
+    $pdo->exec('UPDATE `boardnotes_custom_projects` SET `old_project_id` = `id`');
+    $pdo->exec('ALTER TABLE `boardnotes_entries` ADD `old_project_id` INTEGER');
+    $pdo->exec('UPDATE `boardnotes_entries` SET `old_project_id` = `project_id`');
 
-    $pdo->exec('INSERT INTO `boardnotes_entries_NOPK` (`project_id`, `user_id`, `position`, `is_active`, `date_created`, `date_modified`) VALUES (0, 0, 0, -1, 0, 0)');
-    $pdo->exec('INSERT INTO `boardnotes_entries_NOPK`
-                SELECT * FROM `boardnotes_entries`
-                WHERE `project_id` <> 0 AND `user_id` > 0 AND `position` > 0 AND `is_active` >= 0');
+    // create+insert new shrunk custom projects
+    $pdo->exec('CREATE TABLE IF NOT EXISTS `boardnotes_custom_projects` (
+                    `id` INT NOT NULL AUTO_INCREMENT,
+                    `owner_id` INT NOT NULL DEFAULT 0,
+                    `position` INT,
+                    `project_name` TEXT,
+                    `old_project_id` INTEGER,
+                    PRIMARY KEY(id)
+                ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
+    $pdo->exec('INSERT INTO `boardnotes_custom_projects_NEW`
+				    (`owner_id`, `position`, `project_name`, `old_project_id`)
+                    SELECT `owner_id`, `position`, `project_name`, `old_project_id`
+				    FROM `boardnotes_custom_projects`
+				');
 
-    $pdo->exec('DROP TABLE `boardnotes_entries`');
-    $pdo->exec('CREATE TABLE IF NOT EXISTS `boardnotes_entries` (
-        `id` INT NOT NULL AUTO_INCREMENT,
-        `project_id` INT NOT NULL,
-        `user_id` INT NOT NULL,
-        `position` INT,
-        `is_active` INT,
-        `title` TEXT,
-        `category` TEXT,
-        `description` TEXT,
-        `date_created` INT,
-        `date_modified` INT,
-        PRIMARY KEY(id)
-    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
+    // create+insert new shrunk entries
+    $pdo->exec('CREATE TABLE `boardnotes_entries_NEW` (
+                    `id` INT NOT NULL AUTO_INCREMENT,
+                    `project_id` INT NOT NULL,
+                    `user_id` INT NOT NULL,
+                    `position` INT,
+                    `is_active` INT,
+                    `title` TEXT,
+                    `category` TEXT,
+                    `description` TEXT,
+                    `date_created` INT,
+                    `date_modified` INT,
+                    `old_project_id` INTEGER,
+                    PRIMARY KEY(id)
+                ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
+    $pdo->exec('INSERT INTO `boardnotes_entries_NEW`
+                    (`project_id`, `user_id`, `position`, `is_active`, `date_created`, `date_modified`, `old_project_id`)
+                    VALUES (0, 0, 0, -1, 0, 0, 0)
+                ');
+    $pdo->exec('INSERT INTO `boardnotes_entries_NEW`
+                    (`project_id`, `user_id`, `position`, `is_active`, `title`, `category`, `description`, `date_created`, `date_modified`, `old_project_id`)
+                    SELECT `project_id`, `user_id`, `position`, `is_active`, `title`, `category`, `description`, `date_created`, `date_modified`, `old_project_id`
+                    FROM `boardnotes_entries`
+                    WHERE `project_id` <> 0 AND `user_id` > 0 AND `position` > 0 AND `is_active` >= 0
+                ');
 
-    $pdo->exec('INSERT INTO `boardnotes_entries`
-                (`project_id`, `user_id`, `position`, `is_active`, `title`, `category`, `description`, `date_created`, `date_modified`)
-                SELECT `project_id`, `user_id`, `position`, `is_active`, `title`, `category`, `description`, `date_created`, `date_modified`
-                FROM `boardnotes_entries_NOPK`');
-    $pdo->exec('DROP TABLE `boardnotes_entries_NOPK`');
+    // cross update the reindexed project ids
+    $pdo->exec('UPDATE `boardnotes_entries_NEW` AS `tEntries`, `boardnotes_custom_projects_NEW` AS `tProjects`
+                    SET `tEntries`.`project_id` = -`tProjects`.`id`
+                    WHERE `tEntries`.`old_project_id` = -`tProjects`.`old_project_id`
+                ');
+
+    // drop old_project_id from new tables
+    $pdo->exec('ALTER TABLE boardnotes_custom_projects_NEW DROP old_project_id');
+    $pdo->exec('ALTER TABLE boardnotes_entries_NEW DROP old_project_id');
+
+    // drop old tables
+    $pdo->exec('DROP TABLE boardnotes_custom_projects');
+    $pdo->exec('DROP TABLE boardnotes_entries');
+
+    // rename new tables
+    $pdo->exec('ALTER TABLE boardnotes_custom_projects_NEW RENAME TO boardnotes_custom_projects');
+    $pdo->exec('ALTER TABLE boardnotes_entries_NEW RENAME TO boardnotes_entries');
 }
 
 //////////////////////////////////////////////////
--- Schema/Sqlite.php
+++ Schema/Sqlite.php
@@ -20,73 +20,107 @@ const VERSION = 1;
 //------------------------------------------------
 function version_1(PDO $pdo)
 {
+    // create+insert custom projects
     $pdo->exec('CREATE TABLE IF NOT EXISTS boardnotes_custom_projects (
-        id INTEGER PRIMARY KEY,
-        owner_id INTEGER NOT NULL DEFAULT 0,
-        position INTEGER,
-        project_name TEXT
-    )');
+                    id INTEGER PRIMARY KEY,
+                    owner_id INTEGER NOT NULL DEFAULT 0,
+                    position INTEGER,
+                    project_name TEXT
+                )');
+    $pdo->exec('INSERT INTO boardnotes_custom_projects
+                    (owner_id, position, project_name)
+                    VALUES (0, 1, "Global Notes")
+                ');
+    $pdo->exec('INSERT INTO boardnotes_custom_projects
+                    (owner_id, position, project_name)
+                    VALUES (0, 2, "Global TODO")
+                ');
 
+    // create+insert entries
     $pdo->exec('CREATE TABLE IF NOT EXISTS boardnotes_entries (
-        id INTEGER PRIMARY KEY,
-        project_id INTEGER NOT NULL,
-        user_id INTEGER NOT NULL,
-        position INTEGER,
-        is_active INTEGER,
-        title TEXT,
-        category TEXT,
-        description TEXT,
-        date_created INTEGER,
-        date_modified INTEGER
-    )');
-
-    $pdo->exec('INSERT INTO boardnotes_custom_projects (owner_id, position, project_name) VALUES (0, 1, "Global Notes")');
-    $pdo->exec('INSERT INTO boardnotes_custom_projects (owner_id, position, project_name) VALUES (0, 2, "Global TODO")');
-
-    $pdo->exec('INSERT INTO boardnotes_entries (project_id, user_id, position, is_active, date_created, date_modified) VALUES (0, 0, 0, -1, 0, 0)');
+                    id INTEGER PRIMARY KEY,
+                    project_id INTEGER NOT NULL,
+                    user_id INTEGER NOT NULL,
+                    position INTEGER,
+                    is_active INTEGER,
+                    title TEXT,
+                    category TEXT,
+                    description TEXT,
+                    date_created INTEGER,
+                    date_modified INTEGER
+                )');
+    $pdo->exec('INSERT INTO boardnotes_entries
+                    (project_id, user_id, position, is_active, date_created, date_modified)
+                    VALUES (0, 0, 0, -1, 0, 0)
+                ');
 }
 
 //------------------------------------------------
 function reindexNotesAndLists_1(PDO $pdo)
 {
-    $pdo->exec('CREATE TABLE IF NOT EXISTS boardnotes_entries_NOPK (
-        id INTEGER,
-        project_id INTEGER NOT NULL,
-        user_id INTEGER NOT NULL,
-        position INTEGER,
-        is_active INTEGER,
-        title TEXT,
-        category TEXT,
-        description TEXT,
-        date_created INTEGER,
-        date_modified INTEGER
-    )');
+    // add+update old_project_id
+    $pdo->exec('ALTER TABLE boardnotes_custom_projects ADD old_project_id INTEGER');
+    $pdo->exec('UPDATE boardnotes_custom_projects SET old_project_id = id');
+    $pdo->exec('ALTER TABLE boardnotes_entries ADD old_project_id INTEGER');
+    $pdo->exec('UPDATE boardnotes_entries SET old_project_id = project_id');
 
-    $pdo->exec('INSERT INTO boardnotes_entries_NOPK (project_id, user_id, position, is_active, date_created, date_modified) VALUES (0, 0, 0, -1, 0, 0)');
-    $pdo->exec('INSERT INTO boardnotes_entries_NOPK
-                SELECT * FROM boardnotes_entries
-                WHERE project_id <> 0 AND user_id > 0 AND position > 0 AND is_active >= 0');
+    // create+insert new shrunk custom projects
+    $pdo->exec('CREATE TABLE boardnotes_custom_projects_NEW (
+                    id INTEGER PRIMARY KEY,
+                    owner_id INTEGER NOT NULL DEFAULT 0,
+                    position INTEGER,
+                    project_name TEXT,
+                    old_project_id INTEGER
+                )');
+    $pdo->exec('INSERT INTO boardnotes_custom_projects_NEW
+				    (owner_id, position, project_name, old_project_id)
+                    SELECT owner_id, position, project_name, old_project_id
+				    FROM boardnotes_custom_projects
+				');
 
+    // create+insert new shrunk entries
+    $pdo->exec('CREATE TABLE boardnotes_entries_NEW (
+                    id INTEGER PRIMARY KEY,
+                    project_id INTEGER NOT NULL,
+                    user_id INTEGER NOT NULL,
+                    position INTEGER,
+                    is_active INTEGER,
+                    title TEXT,
+                    category TEXT,
+                    description TEXT,
+                    date_created INTEGER,
+                    date_modified INTEGER,
+                    old_project_id INTEGER
+                )');
+    $pdo->exec('INSERT INTO boardnotes_entries_NEW
+                    (project_id, user_id, position, is_active, date_created, date_modified, old_project_id)
+                    VALUES (0, 0, 0, -1, 0, 0, 0)
+                ');
+    $pdo->exec('INSERT INTO boardnotes_entries_NEW
+                    (project_id, user_id, position, is_active, title, category, description, date_created, date_modified, old_project_id)
+                    SELECT project_id, user_id, position, is_active, title, category, description, date_created, date_modified, old_project_id
+                    FROM boardnotes_entries
+                    WHERE project_id <> 0 AND user_id > 0 AND position > 0 AND is_active >= 0
+                ');
+
+    // cross update the reindexed project ids
+    $pdo->exec('UPDATE boardnotes_entries_NEW AS tEntries
+                    SET project_id = -tProjects.id
+                    FROM boardnotes_custom_projects_NEW AS tProjects
+                    WHERE tEntries.old_project_id = -tProjects.old_project_id
+                ');
+
+    // drop old_project_id from new tables
+    $pdo->exec('ALTER TABLE boardnotes_custom_projects_NEW DROP old_project_id');
+    $pdo->exec('ALTER TABLE boardnotes_entries_NEW DROP old_project_id');
+
+    // drop old tables
+    $pdo->exec('DROP TABLE boardnotes_custom_projects');
     $pdo->exec('DROP TABLE boardnotes_entries');
-    $pdo->exec('CREATE TABLE IF NOT EXISTS boardnotes_entries (
-        id INTEGER PRIMARY KEY,
-        project_id INTEGER NOT NULL,
-        user_id INTEGER NOT NULL,
-        position INTEGER,
-        is_active INTEGER,
-        title TEXT,
-        category TEXT,
-        description TEXT,
-        date_created INTEGER,
-        date_modified INTEGER
-    )');
 
-    $pdo->exec('INSERT INTO boardnotes_entries
-                (project_id, user_id, position, is_active, title, category, description, date_created, date_modified)
-                SELECT project_id, user_id, position, is_active, title, category, description, date_created, date_modified
-                FROM boardnotes_entries_NOPK');
-
-    $pdo->exec('DROP TABLE boardnotes_entries_NOPK');
+    // rename new tables
+    $pdo->exec('ALTER TABLE boardnotes_custom_projects_NEW RENAME TO boardnotes_custom_projects');
+    $pdo->exec('ALTER TABLE boardnotes_entries_NEW RENAME TO boardnotes_entries');
 }
 
 //////////////////////////////////////////////////
